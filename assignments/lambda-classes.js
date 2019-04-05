8180240c0dca7337eae2c83ba8a90b2ef157982f
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-04 19:15:51 -0700

    starting Lambda Classes assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,17 @@
 // CODE here for your Lambda Classes
+
+//classes
+
+class Person {
+    constructor(attrs){
+        this.name = attrs.name;
+        this.location = attrs.location;
+        this.gender = attrs.gender;
+    }
+
+    speak(){
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+//instances and testing
