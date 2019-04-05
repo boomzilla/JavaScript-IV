83b655cdcf959cf743340e11e9158530ae6f7aef
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-04 18:56:03 -0700

    forgot Return statements

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -53,11 +53,12 @@ function GameObject(attrs){
     }
 
     takeDamage() {
+        //debugger;
         return  `${this.name} took damage.`;
     }
 
     destroy() {
-        super.destroy();
+        return super.destroy();
     }
   }
 
@@ -102,11 +103,11 @@ function GameObject(attrs){
         }
 
         destroy() {
-            super.destroy();
+            return super.destroy();
         }
 
         takeDamage() {
-            super.takeDamage();
+            return super.takeDamage();
         }
     }
 
@@ -200,6 +201,7 @@ function GameObject(attrs){
     console.log(archer.language); // Elvish
     console.log(archer.greet()); // Lilith offers a greeting in Elvish.
     console.log(mage.takeDamage()); // Bruce took damage.
+    //debugger;
     console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
   
   
